9232456690471bbb01aec12ecaecff7f30834d63
katyaandrushko <katyaandrushko14@gmail.com>
2023-10-12 17:31:20 +0300

    Delete comments

--- src/components/Brief/Brief.jsx
+++ src/components/Brief/Brief.jsx
@@ -1,40 +1,5 @@
-// import React from 'react'
-// import './brief.scss'
-
-// function Brief() {
-//   return (
-//     <section className="brief">
-//       <div className="rectangable">
-//         <div className="text">
-//           <h1 className="b-title">
-//             Для того, щоб отримати якісну консультацію, заповніть, будь ласка,
-//             коротеньку форму! Це дасть нам змогу дізнатися всі ваші проблеми та
-//             якомога швидше допомогти вирішити їх!
-//           </h1>
-//           {/* <h2 className="b-subtitle">
-//             Це дасть нам змогу дізнатися всі ваші проблеми та якомога швидше
-//             допомогти вирішити їх!
-//           </h2> */}
-//           <h3 className="lorem">
-//             Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
-//             modi optio facere doloribus magni eligendi sunt quis perferendis
-//             deserunt totam qui, perspiciatis, doloremque rerum blanditiis in ex
-//             recusandae sapiente veritatis.
-//           </h3>
-//         </div>
-//         <div className="b-btn">
-//           <button className="button">Заповнити зараз</button>
-//         </div>
-//       </div>
-//     </section>
-//   )
-// }
-
-// export default Brief
-
 import React from 'react'
 import './brief.scss'
-import Contact from '../Contact/Contact'
 
 function Brief() {
   return (
@@ -61,8 +26,6 @@ function Brief() {
           </h3>
         </div>
       </div>
-
-      {/* <div className="b-btn"></div> */}
     </section>
   )
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -19,25 +19,3 @@ function Main() {
 }
 
 export default Main
-
-// import React from 'react'
-// import './main.scss'
-
-// function Main() {
-//   return (
-//     <section className="section-main">
-//       <div className="container">
-//         <div className="main-title">
-//           <h1 className="main-text">
-//             Easy, quality, IT - winning <span>solutions</span> for your business
-//           </h1>
-//         </div>
-//         <div className="mains">
-//           <button className="btn btn-white btn-animate">Замовити зараз</button>
-//         </div>
-//       </div>
-//     </section>
-//   )
-// }
-
-// export default Main
